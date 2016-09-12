161133f5c21775d90e61726a5e8a40be1890d63e
dylan <dylan@dylanwolf.com>
2016-09-11 22:55:54 -0400

    Began work on level editor. Added support for empty spaces on game board.

--- Assets/Scripts/Gameplay/GameState.cs
+++ Assets/Scripts/Gameplay/GameState.cs
@@ -23,151 +23,153 @@ public static class GameState {
     public static Level[] LEVELS = new Level[]
     {
         // Level 1
-        new Level()
-        {
-            Name = "Gears",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.GearGoal }
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GEAR_POOL, 3 }
-            },
-        },
+        //new Level()
+        //{
+        //    Name = "Gears",
+        //    Board = new LevelTile?[,]
+        //    {
+        //        { LevelTile.Engine, null, LevelTile.Empty, null, null },
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.GearGoal },
+        //        { LevelTile.Engine, null, LevelTile.Empty, null, null },
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GEAR_POOL, 3 }
+        //    },
+        //},
 
-        // Level 2
-        new Level()
-        {
-            Name = "Power",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.PowerGoal }
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GEAR_POOL, 1 },
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.REPEATER_POOL, 1 },
-            },
-        },
+        //// Level 2
+        //new Level()
+        //{
+        //    Name = "Power",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.PowerGoal }
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GEAR_POOL, 1 },
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.REPEATER_POOL, 1 },
+        //    },
+        //},
 
-        // Level 3
-        new Level()
-        {
-            Name = "Light",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal }
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 1 },
-            },
-        },
+        //// Level 3
+        //new Level()
+        //{
+        //    Name = "Light",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal }
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 1 },
+        //    },
+        //},
 
-        // Level 4
-        new Level()
-        {
-            Name = "All The Things",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal },
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal }
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 2 },
-                {  GameEngine.REPEATER_POOL, 2 },
-            },
-        },
+        //// Level 4
+        //new Level()
+        //{
+        //    Name = "All The Things",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal },
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal }
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 2 },
+        //        {  GameEngine.REPEATER_POOL, 2 },
+        //    },
+        //},
 
-        // Level 5
-        new Level()
-        {
-            Name = "Split",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.LightGoal, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Engine, LevelTile.Empty, LevelTile.Empty }
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 1 },
-                { GameEngine.MIRROR_POOL, 1 }, 
-            },
-        },
+        //// Level 5
+        //new Level()
+        //{
+        //    Name = "Split",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.LightGoal, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.LightGoal },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Engine, LevelTile.Empty, LevelTile.Empty }
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 1 },
+        //        { GameEngine.MIRROR_POOL, 1 }, 
+        //    },
+        //},
 
-        // Level 6
-        new Level()
-        {
-            Name = "Workaround",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.Empty },
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.LightGoal },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GEAR_POOL, 2 },
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 1 },
-                { GameEngine.REPEATER_POOL, 3 },
-            },
-        },
+        //// Level 6
+        //new Level()
+        //{
+        //    Name = "Workaround",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.Empty },
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.LightGoal },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GEAR_POOL, 2 },
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 1 },
+        //        { GameEngine.REPEATER_POOL, 3 },
+        //    },
+        //},
 
-        // Level 7
-        new Level()
-        {
-            Name = "Collect All Three",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.GearGoal, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.LightGoal },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.PowerGoal, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GEAR_POOL, 2 },
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 1 },
-                { GameEngine.REPEATER_POOL, 3 },
-            },
-        },
+        //// Level 7
+        //new Level()
+        //{
+        //    Name = "Collect All Three",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.GearGoal, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Block, LevelTile.LightGoal },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.PowerGoal, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GEAR_POOL, 2 },
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 1 },
+        //        { GameEngine.REPEATER_POOL, 3 },
+        //    },
+        //},
 
-        // Level 8
-        new Level()
-        {
-            Name = "Open The Way",
-            Board = new LevelTile[,]
-            {
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.LightSwitch, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Door, LevelTile.Empty, LevelTile.LightGoal },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-                { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
-            },
-            Parts = new Dictionary<string, int>()
-            {
-                {  GameEngine.GENERATOR_POOL, 1 },
-                {  GameEngine.LASER_POOL, 1 },
-                { GameEngine.REPEATER_POOL, 2 },
-            },
-            Switches = new SwitchAssociation[]
-            {
-                new SwitchAssociation() { Switch = new int[] { 0, 2 }, Target = new int[] { 2, 4 } }
-            }
-        },
+        //// Level 8
+        //new Level()
+        //{
+        //    Name = "Open The Way",
+        //    Board = new LevelTile[,]
+        //    {
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.LightSwitch, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Engine, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Door, LevelTile.Empty, LevelTile.LightGoal },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //        { LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty, LevelTile.Empty },
+        //    },
+        //    Parts = new Dictionary<string, int>()
+        //    {
+        //        {  GameEngine.GENERATOR_POOL, 1 },
+        //        {  GameEngine.LASER_POOL, 1 },
+        //        { GameEngine.REPEATER_POOL, 2 },
+        //    },
+        //    Switches = new SwitchAssociation[]
+        //    {
+        //        new SwitchAssociation() { Switch = new int[] { 0, 2 }, Target = new int[] { 2, 4 } }
+        //    }
+        //},
     };
 }
---
+++ Assets/Scripts/UI/ToolButton.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToolButton : MonoBehaviour {
+
+    public Sprite Icon;
+    public Sprite Selector;
+    public int? Count;
+    public string PartName;
+    public bool Visible;
+
+    public Image IconRenderer;
+    public Image SelectorRenderer;
+    public Text[] CountText;
+
+    int? lastCount = null;
+    string countText = string.Empty;
+
+    public void RefreshIcon(bool force)
+    {
+        // Get the total count of items, if this is a limited part, and the visibility of the item
+        if (PartName != null && GameState.Parts != null)
+        {
+            if (GameState.Parts.ContainsKey(PartName))
+            {
+                Count = GameState.Parts[PartName];
+                Visible = true;
+            }
+            else if (PartName == GameState.EDITOR_DELETE)
+            {
+                Count = null;
+                Visible = true;
+            }
+            else
+            {
+                Count = null;
+                Visible = false;
+            }
+        }
+        else
+        {
+            Count = null;
+            Visible = false;
+        }
+
+        // Apply to the controls
+        if (IconRenderer != null)
+        {
+            if (Visible)
+                IconRenderer.sprite = Icon;
+            IconRenderer.enabled = Visible;
+        }
+
+        if (SelectorRenderer != null)
+        {
+            if (Visible && (PartName == GameState.EditorSelection))
+                SelectorRenderer.sprite = Selector;
+            SelectorRenderer.enabled = Visible && (PartName == GameState.EditorSelection);
+        }
+
+        if (CountText != null && (Count != lastCount || force))
+        {
+            countText = Count.HasValue ? Count.Value.ToString() : string.Empty;
+            for (int i = 0; i < CountText.Length; i++)
+            {
+                if (Count.HasValue)
+                {
+                    CountText[i].text = countText;
+                }
+                else
+                {
+                    CountText[i].enabled = false;
+                }
+            }
+            lastCount = Count;
+        }
+    }
+
+    public void ButtonClick()
+    {
+        return;
+    }
+}
